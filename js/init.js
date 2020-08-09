06b72cf55386694da75613491031678d5a281d76
Talento <68577325+dtalento@users.noreply.github.com>
2020-08-09 19:12:03 -0300

    Agregada la redireccion al login

--- js/init.js
+++ js/init.js
@@ -40,8 +40,27 @@ var getJSONData = function(url){
     });
 }
 
+//function enviarFormulario(evento){
+  evento.preventDefault();
+  location.href = 'login.html';
+};
+
+var loggeado = sessionStorage.getItem("visitado");
+if (loggeado != 1){
+  window.location.href = "login.html";
+  sessionStorage.setItem("visitado", "1");
+};
+
+
+var redireccion = function() {
+
+  window.location.href = "login.html";
+};
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+
+  redireccion();
+
 });
