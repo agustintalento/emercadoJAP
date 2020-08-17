0c1343cedb73f6350b3fc55e499dfda3d6d1f93a
agustintalento <68829852+agustintalento@users.noreply.github.com>
2020-08-16 22:41:28 -0300

    implemento sing in con google

--- js/login.js
+++ js/login.js
@@ -4,7 +4,21 @@ function detenerEvento(evento) { //detiene el submit del formulario
     window.location.href = 'index.html';
     return true; //envia la información del submit despues de todo
 };
+function onSignIn(googleUser) {
+       
+    var profile = googleUser.getBasicProfile();
+    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+    console.log('Full Name: ' + profile.getName());
+    console.log('Given Name: ' + profile.getGivenName());
+    console.log('Family Name: ' + profile.getFamilyName());
+    console.log("Image URL: " + profile.getImageUrl());
+    console.log("Email: " + profile.getEmail());
 
+    //inicio de sesión con google id
+    sessionStorage.setItem('logueado', 'true');
+    window.location.href = 'index.html';
+
+}
 
 document.getElementById("log in").addEventListener("submit", detenerEvento);
    
